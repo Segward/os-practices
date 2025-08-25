fbb995960a0e07f87586a62a2e873655b72ede2a
Segward <gustask@stud.ntnu.no>
2025-08-25 13:51:28 +0200

    Overkill on printx

--- practice-1/src/printx.c
+++ practice-1/src/printx.c
@@ -4,6 +4,31 @@
 
 #define MAX_LENGTH 4096
 
+int foo(int n, int (*func)(const char*, int)) {
+  if (n <= 0) {
+    printf("Error: n must be a positive integer.\n");
+    return 1;
+  }
+
+  char input[MAX_LENGTH];
+  printf("Enter a string: ");     
+  fgets(input, MAX_LENGTH, stdin);
+  return func(input, n);
+}
+
+int bar(const char* s, int n) {
+  if (s == NULL) {
+    printf("Error: String is NULL.\n");
+    return 1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    printf("%s", s);
+  }
+
+  return 0;
+}
+
 int main(int argc, const char** argv) {
     if (argc < 2) {
       printf("Usage: %s <integer>\n", argv[0]);
@@ -24,13 +49,7 @@ int main(int argc, const char** argv) {
       return 1;
     }
 
-    char input[MAX_LENGTH];
-    printf("Enter a string: ");     
-    fgets(input, MAX_LENGTH, stdin);
-
-    for (int i = 0; i < number; i++) {
-      printf("%s", input);
-    }
-
-    return 0;
+    int (*func_ptr)(const char*, int) = &bar;
+    int r = foo(number, func_ptr);
+    return r;
 }
